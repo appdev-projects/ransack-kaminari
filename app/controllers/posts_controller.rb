57632805b91fa8f129ba9fe64f75c77fc68108c4
Ian <heratyian@gmail.com>
2023-05-10 16:44:19 +0000

    Change list to just @posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,7 @@ class PostsController < ApplicationController
   before_action :force_user_sign_in, only: [:new, :edit, :create, :update, :destroy]
 
   def index
-    matching_posts = Post.all
-
-    @list_of_posts = matching_posts.order({ :created_at => :desc })
+    @posts = Post.all.order({ :created_at => :desc })
 
     render({ :template => "posts/index.html.erb" })
   end
